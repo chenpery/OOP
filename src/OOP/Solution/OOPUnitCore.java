f08edff60167feab9592906f5919b2973b905e7c
Amit Rush <rushamit@cmapus.technion.ac.il>
2022-01-12 01:48:02 +0200

    faild: 31 (at least one is negotiable)

--- src/OOP/Solution/OOPUnitCore.java
+++ src/OOP/Solution/OOPUnitCore.java
@@ -18,17 +18,16 @@ public class OOPUnitCore {
 //    private static Annotation[] ;
 
     /* if the input objects is not equal throw exception*/
-    public static void assertEquals(Object expected, Object actual){
-        if(!expected.equals(actual)){
-            throw new OOPAssertionFailure(expected,actual);
+    public static void assertEquals(Object expected, Object actual) {
+        if (!expected.equals(actual)) {
+            throw new OOPAssertionFailure(expected, actual);
         }
     }
 
     /* fail test method via trow exception */
-    public static void fail(){
+    public static void fail() {
         throw new OOPAssertionFailure();
     }
-
 
 
     private static ArrayList<Object> backupFileds(Object new_instance, Field[] fields) {
@@ -55,16 +54,15 @@ public class OOPUnitCore {
                 }
             } catch (Exception e) {
                 return null;
-            }
-            finally {
+            } finally {
                 backedFields.add(copy);
             }
         }
         return backedFields;
     }
 
-    public static OOPTestSummary runClassHelper(Class<?> testClass, String tag){
-        if(testClass == null || (!testClass.isAnnotationPresent(OOPTestClass.class))) {
+    public static OOPTestSummary runClassHelper(Class<?> testClass, String tag) {
+        if (testClass == null || (!testClass.isAnnotationPresent(OOPTestClass.class))) {
             throw new IllegalArgumentException();
         }
         Map<String, OOPResult> mapResults = new HashMap<>();
@@ -79,10 +77,18 @@ public class OOPUnitCore {
             ArrayList<Method> afterMethods = new ArrayList<>();
 
             // collect OOPSetup, OOPBefore, OOPAfter methods from father to sun
+            // and collecting OOPTest methods with/without ORDER/tag
             Class<?> className = new_instance.getClass();
-            while( className != null){
+            OOPTestClass classAnnotation = testClass.getAnnotation(OOPTestClass.class);
+            OOPTestClass.OOPTestClassType ordered = OOPTestClass.OOPTestClassType.UNORDERED;
+            if (classAnnotation != null) {
+                ordered = classAnnotation.value();
+            }
+            int i = 1;
+            Map<Integer, Method> temp = new HashMap<>();
+            while (className != null) {
                 Method[] methods = className.getDeclaredMethods(); // not including inherited methods
-                for(Method m : methods) {
+                for (Method m : methods) {
                     if (m.isAnnotationPresent(OOPSetup.class)) {
                         setupMethods.add(m);
                         continue;
@@ -93,49 +99,38 @@ public class OOPUnitCore {
                     }
                     if (m.isAnnotationPresent(OOPAfter.class)) {
                         afterMethods.add(m);
+                        continue;
+                    }
+                    if (m.isAnnotationPresent(OOPTest.class)) {
+                        OOPTest annotation = m.getAnnotation(OOPTest.class);
+                        if (annotation != null) {
+                            if (Objects.equals(OOPTestClass.OOPTestClassType.ORDERED, ordered)) {
+                                // get the right order for the method
+                                i = annotation.order();
+                            }
+                            if (tag == null) {
+                                temp.put(i, m); //if not ordered, we have the initial i = 0
+                            } else {
+                                // get only tagged methods
+                                String thisMethodTag = annotation.tag();
+                                if (thisMethodTag.contains(tag)) {
+                                    temp.put(i, m);
+                                }
+                            }
+                            i++;
+                        }
                     }
                 }
                 className = className.getSuperclass();
             }
+            Stream<Map.Entry<Integer, Method>> stream = temp.entrySet().stream().sorted(Map.Entry.comparingByKey());
+            Map<Integer, Method> testMethods = stream.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
-            // collecting (and ordering) the Test-methods (with/without tag)
-            OOPTestClass classAnnotation = testClass.getAnnotation(OOPTestClass.class);
-            OOPTestClass.OOPTestClassType ordered = OOPTestClass.OOPTestClassType.UNORDERED;
-            if(classAnnotation != null) {
-                ordered = classAnnotation.value();
-            }
-            int i = 1;
-            Map<Integer,Method> temp = new HashMap<>();
-            Method[] thisClassMethods = testClass.getDeclaredMethods(); // not including inherited methods
-            for(Method m : thisClassMethods){
-                if(m.isAnnotationPresent(OOPTest.class)){
-                    OOPTest annotation = m.getAnnotation(OOPTest.class);
-                    if(annotation != null) {
-                        if (Objects.equals(OOPTestClass.OOPTestClassType.ORDERED, ordered)) {
-                            // get the right order for the method
-                            i = annotation.order();
-                        }
-                        if (tag == null) {
-                            temp.put(i,m); //if not ordered, we have the initial i = 0
-                        }
-                        else{
-                            // get only tagged methods
-                            String thisMethodTag = annotation.tag();
-                            if (Objects.equals(thisMethodTag, tag)){
-                                temp.put(i,m);
-                            }
-                        }
-                        i++;
-                    }
-                }
-            }
-            Stream<Map.Entry<Integer,Method>> stream = temp.entrySet().stream().sorted(Map.Entry.comparingByKey());
-            Map<Integer,Method> testMethods = stream.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
 
             // invoke OOPSetup methods
             Collections.reverse(setupMethods);
-            for (Method m: setupMethods) {
+            for (Method m : setupMethods) {
                 m.invoke(new_instance, null);
             }
 
@@ -143,8 +138,8 @@ public class OOPUnitCore {
             // getting the OOPExceptionRule fields to check results later
             Field[] fields = testClass.getDeclaredFields();
             Field expectedException = null;
-            for (Field f: fields ) {
-                if (f.isAnnotationPresent(OOPExceptionRule.class)){
+            for (Field f : fields) {
+                if (f.isAnnotationPresent(OOPExceptionRule.class)) {
                     expectedException = f;
                     break;
                 }
@@ -155,7 +150,7 @@ public class OOPUnitCore {
             boolean failed_before_after = false;
             Collections.reverse(beforeMethods);
             OOPResult result = null;
-            for (Method m: testMethods.values()) {
+            for (Method m : testMethods.values()) {
                 // invoke OOPBefore that include the current method m
                 for (Method bm : beforeMethods) {
                     if (failed_before_after) break;
@@ -181,31 +176,35 @@ public class OOPUnitCore {
                     }
                 }
 
-                if(failed_before_after) {
+                if (failed_before_after) {
                     mapResults.put(m.getName(), result);
-                }
-                else {
+                } else {
                     try {
                         // invoke current OOPTest method (m)
                         m.invoke(new_instance, null);
                         result = new OOPResultImpl(OOPResult.OOPTestResult.SUCCESS, null);
                         mapResults.put(m.getName(), result);
-                    } catch (OOPAssertionFailure e) {
-                        result = new OOPResultImpl(OOPResult.OOPTestResult.FAILURE, e.getMessage());
-                        mapResults.put(m.getName(), result);
+//                    } catch (OOPAssertionFailure e) {
+//                        result = new OOPResultImpl(OOPResult.OOPTestResult.FAILURE, e.getMessage());
+//                        mapResults.put(m.getName(), result);
                     } catch (Exception e) {
-                        if (expectedException != null) {
-                            OOPExpectedException err = (OOPExpectedException) expectedException.get(new_instance);
-                            if (err.getExpectedException() == null) {
-                                result = new OOPResultImpl(OOPResult.OOPTestResult.ERROR, e.getClass().toString());
-                            } else {
-                                if(expectedException.getType() == OOPExpectedException.class && err.assertExpected(e)){
-                                    result = new OOPResultImpl(OOPResult.OOPTestResult.SUCCESS, null);
-                                }
-                                else{
-                                    OOPExceptionMismatchError mismatch = new OOPExceptionMismatchError(err.getExpectedException(), e.getClass());
-                                    result = new OOPResultImpl(OOPResult.OOPTestResult.EXPECTED_EXCEPTION_MISMATCH, mismatch.getMessage());
-                                }
+                        Throwable noWarper = e.getCause(); // now we have the real exception the method threw
+                        Class<?> expClass = noWarper.getClass();
+                        if (expClass.equals(OOPAssertionFailure.class)) {
+                            result = new OOPResultImpl(OOPResult.OOPTestResult.FAILURE, e.getMessage());
+                            mapResults.put(m.getName(), result);
+                        } else {
+                            if (expectedException != null) {
+                                OOPExpectedException err = (OOPExpectedException) expectedException.get(new_instance);
+                                if (err.getExpectedException() == null) {
+                                    result = new OOPResultImpl(OOPResult.OOPTestResult.ERROR, expClass.toString());
+                                } else {
+                                    if (expectedException.getType() == OOPExpectedException.class && err.assertExpected((Exception) noWarper)) {
+                                        result = new OOPResultImpl(OOPResult.OOPTestResult.SUCCESS, null);
+                                    } else {
+                                        OOPExceptionMismatchError mismatch = new OOPExceptionMismatchError(err.getExpectedException(), (Class<? extends Exception>) expClass);
+                                        result = new OOPResultImpl(OOPResult.OOPTestResult.EXPECTED_EXCEPTION_MISMATCH, mismatch.getMessage());
+                                    }
 //                                if (err.getExpectedException() != e.getClass() || !err.assertExpected(e)) {
 //                                    OOPExceptionMismatchError mismatch = new OOPExceptionMismatchError(err.getExpectedException(), e.getClass());
 //                                    result = new OOPResultImpl(OOPResult.OOPTestResult.EXPECTED_EXCEPTION_MISMATCH, mismatch.getMessage());
@@ -214,42 +213,42 @@ public class OOPUnitCore {
 //                                    result = new OOPResultImpl(OOPResult.OOPTestResult.SUCCESS, null);
 //                                    mapResults.put(m.getName(), result);
 //                                }
+                                }
+                            } else {
+                                result = new OOPResultImpl(OOPResult.OOPTestResult.ERROR, e.getClass().toString());
                             }
-                        }
-                        else{
-                            result = new OOPResultImpl(OOPResult.OOPTestResult.ERROR, e.getClass().toString());
                         }
 
                     }
                 }
-                    // invoke OOPAfter that include the current method m
-                    for (Method am : afterMethods) {
-                        if(failed_before_after) break;
-                        OOPAfter afterAnnotation = am.getAnnotation(OOPAfter.class);
-                        String[] methodsArray = afterAnnotation.value();
-                        for (String str : methodsArray) {
-                            if (Objects.equals(str, m.getName())) {
-                                //backup the fields of new_instance (the object of our tested class) before invoking OOPBefore
-                                ArrayList<Object> backedFields = backupFileds(new_instance, fields);
-                                try {
-                                    am.invoke(new_instance, null);
+                // invoke OOPAfter that include the current method m
+                for (Method am : afterMethods) {
+                    if (failed_before_after) break;
+                    OOPAfter afterAnnotation = am.getAnnotation(OOPAfter.class);
+                    String[] methodsArray = afterAnnotation.value();
+                    for (String str : methodsArray) {
+                        if (Objects.equals(str, m.getName())) {
+                            //backup the fields of new_instance (the object of our tested class) before invoking OOPBefore
+                            ArrayList<Object> backedFields = backupFileds(new_instance, fields);
+                            try {
+                                am.invoke(new_instance, null);
 
-                                    break;
-                                } catch (Exception e) { // OOPBefore  method failed so need to restore the value of field
-                                    for (int j = 0; j < fields.length; j++) {
-                                        fields[j].set(new_instance, (backedFields.get(j)));
-                                    }
-                                    result = new OOPResultImpl(OOPResult.OOPTestResult.ERROR, "");
-                                    failed_before_after = true;
-                                    break;
-                                    // I think there should be another break for the outer loop (for the am loop)
+                                break;
+                            } catch (Exception e) { // OOPBefore  method failed so need to restore the value of field
+                                for (int j = 0; j < fields.length; j++) {
+                                    fields[j].set(new_instance, (backedFields.get(j)));
                                 }
+                                result = new OOPResultImpl(OOPResult.OOPTestResult.ERROR, "");
+                                failed_before_after = true;
+                                break;
+                                // I think there should be another break for the outer loop (for the am loop)
                             }
                         }
                     }
+                }
 
-                if (result != null){
-                    mapResults.put(m.getName(),result);
+                if (result != null) {
+                    mapResults.put(m.getName(), result);
                 }
             }
 
